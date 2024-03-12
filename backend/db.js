ddd47c676ea4e69aa712e595d6c89fd87d66e7d4
saurav1812 <shorya181299@gmail.com>
2024-03-12 14:19:58 +0530

    initial commit

---
+++ backend/db.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+    minLength: 3,
+    maxLength: 30,
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 6,
+  },
+  firstname: {
+    type: String,
+    required: true,
+    trim: true,
+    minLength: 50,
+  },
+  lastname: {
+    type: String,
+    required: true,
+    trim: true,
+    minLength: 50,
+  },
+});
+
+const user = mongoose.model('user',userSchema);
+
+module.exports = {
+    user
+};
